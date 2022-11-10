38ecb89b1a4814b17ecbfe573ae8c36a7787f6ef
LordKraid <the.lord.kraid@gmail.com>
2022-11-10 17:54:50 +0200

    add project

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ window.addEventListener('scroll', function() {
     if (window.pageYOffset > 0) {document.querySelector('header').classList.add('scrolled')}
     else {document.querySelector('header').classList.remove('scrolled')}
   });
+
+
+let imgBlock = document.querySelector('.projects-lorem__img-block')
+
+for(let i = 1; i < 10; i++){
+  imgBlock.children[i].style.marginTop = i * -15 + 'px'
+}
