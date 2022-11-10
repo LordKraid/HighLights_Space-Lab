8195aede222fbc786a678f2dd9d73ddacde90e8c
LordKraid <the.lord.kraid@gmail.com>
2022-11-10 19:31:18 +0200

    add preloader

--- js/script.js
+++ js/script.js
@@ -9,3 +9,22 @@ let imgBlock = document.querySelector('.projects-lorem__img-block')
 for(let i = 1; i < 10; i++){
   imgBlock.children[i].style.marginTop = i * -15 + 'px'
 }
+
+procent = document.querySelector('.preloader__procent')
+
+let preloader = setInterval(function() {
+  procent.innerHTML = ++procent.innerHTML
+
+  if (procent.innerHTML == 100) clearInterval(preloader)
+}, 10)
+
+setTimeout(function() {
+  document.querySelector('.preloader__load').classList.add('hiden')
+  document.querySelector('.preloader__downloaded').classList.add('visible')
+}, 3000)
+
+setTimeout(function() {
+  document.querySelector('.preloder').style.transform = "translate(0, -100vh)"
+
+  document.body.style.overflowY = 'visible'
+}, 4000)
